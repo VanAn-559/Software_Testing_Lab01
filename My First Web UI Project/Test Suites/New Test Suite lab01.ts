a4a224d6281b587da84c996cc7e2721854b21147
Cocerswi@123 <nguyenvanan8398@gmail.com>
2020-09-24 17:54:52 +0700

    avc

---
+++ My First Web UI Project/Test Suites/New Test Suite lab01.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite lab01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>dada2f72-81cb-442d-b9d2-151502851531</testSuiteGuid>
+   <testCaseLink>
+      <guid>28db9eda-10a1-415e-a397-d9cfff84095d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0531a365-9374-4df4-9718-30f856c536db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9b9d30a0-3541-4202-abb3-c1ac775e0448</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 03</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6985c374-01cb-4fc8-8903-38af78655ff9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d927cd53-da54-413b-abbe-e9dab49529ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 05</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
